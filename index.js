a95ed432a0fd3d34d5016358ddd862d5c0705dc7
Steve Dossin <steve.dossin@gmail.com>
2019-01-22 11:18:44 +0100

    add loading

--- index.js
+++ index.js
@@ -7,38 +7,58 @@ var chalk = require('chalk');//COLORS
 const ora = require('ora');//SPINNER
 
 
-chalk.green(figlet('BECODE', function(err, data) {
+chalk.green(figlet('Mail-checker @ BECODE \n', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
     console.log(data)
+    console.log("\n")
 }))
 
 
 const [,, ...args] = process.argv;
 
 if (validator.validate(`${args}`) == true){
-    console.log(`L'adresse ${args} est valide`);
+    console.log(chalk.bold(`${args} is valid.`));
+
+    let spinner = ora(`Looking for breaches from ${args} \n`)
+
+    spinner.start()
+
+    setTimeout(() => {
+        spinner.text = `Looking for breaches from ${args} \n`
+    })
+
+
+
+    axios({
+        method: 'get',
+        url: `https://haveibeenpwned.com/api/v2/breachedaccount/${args}`,
+        headers: {
+        'User-Agent': 'pwndmail'
+        },
+    }).then(res => { 
+        spinner.stop()
+        spinner.clear()
+        console.log(chalk.bold.bgRed("You've been powned by"))
+
+
+
+        res.data.forEach(function (breach){
+            console.log(`- ${breach.Name}`)
+            console.log(`  ${breach.Domain}`)
+        })
+
+        
+        
+    }).catch(err => {
+        const log = chalk.yellow(err)
+        spinner.stop()
+        spinner.clear()
+ });
 
 }else{
-    console.log(`[!!!] L'adresse ${args} est invalide`)
+    console.log(`!!! ${args} is invalid.`)
 }
-
-axios({
-    method: 'get',
-    url: `https://haveibeenpwned.com/api/v2/breachedaccount/${args}`,
-    headers: {
-       'User-Agent': 'pwndmail'
-    },
- }).then(res => {
-    console.log(chalk.bgRed("You've been powned by"))
-
-     res.data.forEach(function (breach){
-        console.log(breach.Domain)
-     })
-    
- }).catch(err => {
-    const log = chalk.yellow(err)
- });
